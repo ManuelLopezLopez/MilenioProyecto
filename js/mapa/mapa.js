aa2343f5bfee75ea26a7c274a7adda31943d6113
Javier Lopez Morales <javizapillo2@gmail.com>
2021-05-13 22:52:20 +0200

    Cambio de imagenes completo

--- js/mapa/mapa.js
+++ js/mapa/mapa.js
@@ -1,15 +1,16 @@
 function cambioImagen(valor) {
-    document.getElementsByClassName('img1')[0].style.display = "block";
-    document.getElementsByClassName('img2')[0].style.display = "block";
-    document.getElementsByClassName('textoImagen')[0].style.display = "block";
+    document.getElementsByClassName('informacion')[0].style.display = "flex";
     document.getElementsByClassName('mapaImg')[0].src = "../img/mapa/mapa/"+ valor +".jpg";
     document.getElementsByClassName('img1')[0].src = "../img/mapa/descripcion/"+ valor +"1.jpg";
     document.getElementsByClassName('img2')[0].src = "../img/mapa/descripcion/"+ valor +"2.jpg";
 
-    if(valor == "inicial" || valor == "puertas"){
-        document.getElementsByClassName('img1')[0].style.display = "none";
-        document.getElementsByClassName('img2')[0].style.display = "none";
-        document.getElementsByClassName('textoImagen')[0].style.display = "none";
+    if(valor == "inicial" || valor == "puertas" || valor == "puertas_musalla" || valor == "puertas_medina"){
+        document.getElementsByClassName('informacion')[0].style.display = "none";
+
+    }
+
+    if(valor == "medina_alcazaba" || valor == "al_mutasin"){
+        document.getElementsByClassName('img2')[0].src = "../img/mapa/descripcion/alcazaba2.jpg";
     }
 
 }
